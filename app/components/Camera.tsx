96d29203879f07d09765bdff2d392f1ba1d014b6
vanderkilu <vanderkiliku@gmail.com>
2020-05-14 23:10:13 +0000

    implement photo capture functionality

--- app/components/Camera.tsx
+++ app/components/Camera.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import { Camera } from "expo-camera";
 import * as Permissions from "expo-permissions";
 
@@ -7,6 +7,8 @@ const CameraView: React.FC<{}> = () => {
   const [hasPermission, setHasPermission] = useState<boolean | null>(null);
   const [cameraRef, setCameraRef] = useState<Camera | null>(null);
   const [photo, setPhoto] = useState();
+  const [isCameraMode, setIsCameraMode] = useState(true);
+  const [isProcessing, setIsProcessing] = useState(false);
   const type = Camera.Constants.Type.back;
   useEffect(() => {
     (async () => {
@@ -18,8 +20,8 @@ const CameraView: React.FC<{}> = () => {
   const takeShot = async () => {
     if (cameraRef) {
       const photo = await cameraRef.takePictureAsync();
-      console.log(photo);
       setPhoto(photo);
+      setIsCameraMode(false);
     }
   };
 
@@ -32,29 +34,48 @@ const CameraView: React.FC<{}> = () => {
     );
   return (
     <View style={{ flex: 1 }}>
-      <Camera
-        style={{ flex: 1, flexDirection: "row", justifyContent: "center" }}
-        type={type}
-        ref={ref => setCameraRef(ref)}
-      >
-        <View style={{ alignSelf: "flex-end" }}>
-          <TouchableOpacity style={{ alignSelf: "center" }} onPress={takeShot}>
-            <View
-              style={{
-                borderWidth: 5,
-                borderRadius: 100 / 2,
-                borderColor: "grey",
-                height: 100,
-                width: 100,
-                display: "flex",
-                backgroundColor: "#ffffff",
-                justifyContent: "center",
-                alignItems: "center"
-              }}
-            />
-          </TouchableOpacity>
+      {isCameraMode ? (
+        <Camera
+          style={{ flex: 1, flexDirection: "row", justifyContent: "center" }}
+          type={type}
+          ref={ref => setCameraRef(ref)}
+        >
+          <View style={{ alignSelf: "flex-end" }}>
+            <TouchableOpacity
+              style={{ alignSelf: "center" }}
+              onPress={takeShot}
+            >
+              <View
+                style={{
+                  borderWidth: 5,
+                  borderRadius: 100 / 2,
+                  borderColor: "grey",
+                  height: 100,
+                  width: 100,
+                  display: "flex",
+                  backgroundColor: "#ffffff",
+                  justifyContent: "center",
+                  alignItems: "center"
+                }}
+              />
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      ) : (
+        <View
+          style={{
+            opacity: 0.6
+          }}
+        >
+          <Image
+            source={{ uri: photo.uri }}
+            style={{
+              width: "100%",
+              height: "100%"
+            }}
+          />
         </View>
-      </Camera>
+      )}
     </View>
   );
 };
