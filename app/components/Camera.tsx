fcaf6f099f367c1527320f0d0433017d2a66ecfb
vanderkilu <vanderkiliku@gmail.com>
2020-05-15 02:51:15 +0000

    add activity loader

--- app/components/Camera.tsx
+++ app/components/Camera.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import { Camera } from "expo-camera";
 import * as Permissions from "expo-permissions";
+import Loader from "./Loader";
 import APIService from "../api";
 
 const CameraView: React.FC<{}> = () => {
@@ -10,6 +11,7 @@ const CameraView: React.FC<{}> = () => {
   const [photo, setPhoto] = useState();
   const [isCameraMode, setIsCameraMode] = useState(true);
   const [isProcessing, setIsProcessing] = useState(false);
+  const [prediction, setPrediction] = useState("");
   const type = Camera.Constants.Type.back;
   useEffect(() => {
     (async () => {
@@ -23,7 +25,10 @@ const CameraView: React.FC<{}> = () => {
       const photo = await cameraRef.takePictureAsync({ skipProcessing: true });
       setPhoto(photo);
       setIsCameraMode(false);
+      setIsProcessing(true);
       const response = await APIService.post(photo);
+      setIsProcessing(false);
+      setPrediction(response.prediction);
     }
   };
 
@@ -66,7 +71,8 @@ const CameraView: React.FC<{}> = () => {
       ) : (
         <View
           style={{
-            opacity: 0.6
+            opacity: 0.6,
+            position: "relative"
           }}
         >
           <Image
@@ -76,6 +82,43 @@ const CameraView: React.FC<{}> = () => {
               height: "100%"
             }}
           />
+          {isProcessing && (
+            <View
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Loader />
+            </View>
+          )}
+          {prediction !== "" && (
+            <View
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 30,
+                  color: "#a2edff"
+                }}
+              >
+                {prediction}
+              </Text>
+            </View>
+          )}
         </View>
       )}
     </View>
